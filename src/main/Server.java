fbdb5c59f8dd2af0aec719c5ed70c5102d3ca273
Miguel <nov12164@uvg.edu.gt>
2016-02-04 20:18:48 -0600

    Notas agregadas en Server para hacer pruebas con Telnet

--- src/main/Server.java
+++ src/main/Server.java
@@ -5,5 +5,12 @@ public class Server {
 	public static void main(String[] args) {
 		System.out.println("Hola chechita!");
 	}
+	
+	/*
+	 * Tips: Recuerde las pruebas realizadas en clase. 
+	 * Haga pruebas contra un servidor SMTP y observe la secuencia de comandos. 
+	 * Puede hacer pruebas para analizar el flujo de datos utilizando el siguiente servidor:
+	 * postini.com.s200a1.psmtp.com
+	 */
 
 }
